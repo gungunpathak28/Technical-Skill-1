d1403698748b292af580f84a4d6800734cbeb75e
Gungun pathak <gungunpathak.2006@gmail.com>
2026-08-31 14:55:58 +0530

    Add files via upload

---
+++ FirstNonRepeatingChar.java
@@ -0,0 +1,24 @@
+public class FirstNonRepeatingChar {
+
+    public static void main(String[] args) {
+
+        String str = "SWISS";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("First Non-Repeating Character: " + str.charAt(i));
+                break;
+            }
+        }
+    }
+}
---
+++ max.java
@@ -0,0 +1,24 @@
+
+public class max {
+
+	public static void main(String[] args) {
+		int arr[] = {1, 0, 1, 1, 0, 1, 1, 1, 1, 0};
+		int count = 0;
+		int max = 0;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]==1) {
+				count++;
+				
+			}else {
+				count = 0;
+			}
+			if(count>max) {
+				max = count;
+			}
+		}		
+		System.out.println(max);
+		
+
+	}
+
+}
