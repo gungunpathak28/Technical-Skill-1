cbd6ff23d43fd5988d80137426b033a81fb8e839
Gungun pathak <gungunpathak.2006@gmail.com>
2026-09-05 21:03:14 +0530

    Fix formatting issues in CountElements.java

--- CountElements.java
+++ CountElements.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class CountElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
